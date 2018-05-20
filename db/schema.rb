f2892d521b92455729202a66b90d85692375434b
Denise Pendleton <dpendl1@hotmail.com>
2018-05-20 23:19:01 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520223216) do
+ActiveRecord::Schema.define(version: 20180520231845) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
 end
